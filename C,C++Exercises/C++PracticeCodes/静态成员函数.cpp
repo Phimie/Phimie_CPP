6cc50d06bf04ef112218caa4484f6ead01e8d56d
Phimie <Phimiee@outlook.com>
2025-05-28 22:21:32 +0800

    Update

--- C,C++Exercises/C++PracticeCodes/静态成员函数.cpp
+++ C,C++Exercises/C++PracticeCodes/静态成员函数.cpp
@@ -4,10 +4,25 @@
 class Student
 {
 public:
+    static void func() // 静态成员函数
+    {
+        S_age = 100;
+        std::cout << "static void func的调用" << std::endl;
+    }
+
+    static int S_age; // 静态成员变量
 };
+
+int Student::S_age = 0;
 
 void test01()
 {
+    std::cout << "通过对象访问func" << std::endl;
+    Student s1;
+    s1.func();
+
+    std::cout << "通过类名进行访问func" << std::endl;
+    Student::func();
 }
 
 int main()
@@ -15,3 +30,6 @@ int main()
     test01();
     return 0;
 }
+
+// 重点总结:
+// 静态成员函数只能访问静态成员变量
